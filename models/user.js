6775e8b3e15e6de5f8ec31480e9701c4b7277055
ricky-kiva <ricky.kiva@gmail.com>
2024-05-09 12:05:04 +0700

    feat(model): add `User` model

---
+++ models/user.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-underscore-dangle */
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+})
+
+userSchema.set('toJSON', {
+  transform: (_, returnObj) => {
+    returnObj.id = returnObj._id.toString()
+    delete returnObj.password
+    delete returnObj.__v
+    delete returnObj._id
+  },
+})
+
+module.export = mongoose.model('User', userSchema)
