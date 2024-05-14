b62b51159b50162f880da4b155bf82fffbb73f37
ricky-kiva <ricky.kiva@gmail.com>
2024-05-14 18:31:36 +0700

    feat(middleware): add res. to handle exception

--- utils/middleware.js
+++ utils/middleware.js
@@ -5,6 +5,12 @@ const errorHandler = (err, _, res, next) => {
 
   if (err.name === 'ValidationError') {
     return res.status(400).send({ error: err.message })
+  } if (err.name === 'JsonWebTokenError') {
+    return res.status(401).send({ error: 'token invalid' })
+  } if (err.name === 'TokenExpiredError') {
+    return res.status(401).send({ error: 'token expired' })
+  } if (err.name === 'MongoServerError' && err.message.includes('E11000 duplicate key error')) {
+    return res.status(400).json({ error: 'expected `username` to be unique' })
   }
 
   next(err)
